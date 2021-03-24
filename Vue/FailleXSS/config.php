babb5cc240d539347439cb247cb5c4717eda720e
KillianFournier <60177401+KillianFournier@users.noreply.github.com>
2021-03-24 20:52:55 +0100

    database

--- Vue/FailleXSS/config.php
+++ Vue/FailleXSS/config.php
@@ -1,10 +1,19 @@
-<?php
-
-try
-{
-	$bdd = new PDO('mysql:host=185.98.131.148;dbname=veill1513693;charset=utf8', 'veill1513693', 'Veilletechno42');
-}
-catch(Exception $e)
-{
-        die('Erreur : '.$e->getMessage());
+<?php
+
+
+
+try
+
+{
+
+	$bdd = new PDO('mysql:host=185.98.131.148;dbname=db;charset=utf8', 'db', 'db');
+
+}
+
+catch(Exception $e)
+
+{
+
+        die('Erreur : '.$e->getMessage());
+
 }
